3af7d515278aff053d8fb53f851cff5d073719f9
jain-ari <arihant92@gmail.com>
2017-03-12 00:08:34 -0500

    Update limit.js

--- practical-exercises/part-1/limit.js
+++ practical-exercises/part-1/limit.js
@@ -6,16 +6,16 @@ var textarea = document.getElementById('textarea');
 var main = textarea.oninput = function() {
   //Getting current length
   var length = textarea.value.length;
-  label.innerHTML = "Remaining Chars: " + (10 - length);
+  label.innerHTML = "Remaining Chars: " + (100 - length);
 
   //Check for 5 characters and change color
-  if ((10 - length) < 6) {
+  if ((100 - length) < 6) {
     label.style.color = "red";
   } else {
     label.style.color = "black";
   }
   //Restricting input
-  if (10 - length <= 0) {
+  if (100 - length <= 0) {
     textarea.onkeypress = function() {
       return false;
     }
@@ -41,7 +41,7 @@ function handlePaste(e) {
   pastedData = clipboardData.getData('Text');
 
   // Checking length of data being pasted
-  if (pastedData.length >= 10) {
+  if (pastedData.length >= 100) {
     alert('Pasted data out of bounds')
   }
 }
